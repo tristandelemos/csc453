54accc3c033ccfac56f64052287b6a17298ac701
tristandelemos <71288767+tristandelemos@users.noreply.github.com>
2023-05-12 13:00:44 -0700

    read in matrix

--- schedSim.py
+++ schedSim.py
@@ -5,10 +5,19 @@ Scheduling Simulator
 def main():
     """
     """
+    matrix = []
     # check for valid.txt file
     file = open("test.txt", "r")
+    SIZE = 0
+    for x in file:
+        a = []
+        a.append(x[0])
+        a.append(x[2])
+        matrix.append(a)
+        SIZE += 1
 
-    
+    print(matrix)
+    print(SIZE)
 
     # put what was in.txt file into an array array
 
