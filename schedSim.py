fbed81608346a0586e9cffc97d44ce1b3a50ac6b
tristandelemos <71288767+tristandelemos@users.noreply.github.com>
2023-05-12 12:47:10 -0700

    py

--- schedSim.py
+++ schedSim.py
@@ -5,13 +5,28 @@ Scheduling Simulator
 def main():
     """
     """
+    # check for valid.txt file
+    file = open("test.txt", "r")
+
     
+
+    # put what was in.txt file into an array array
+
+    # sort matrix to base config
+
+    # check for algorithm
+
+    # check for quantum
+
+    # call whichever scheduling algorithm was chosen
+
+
+
+
 
 if __name__ == "__main__":
     """
-
     """
-
     main()
 
 
