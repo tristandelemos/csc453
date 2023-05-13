657222bfb4a0071afae917cc0359e513152e1456
EternalLabrynth <eternallabrynth@gmail.com>
2023-05-12 17:23:52 -0700

    updated main

--- schedSim.py
+++ schedSim.py
@@ -88,7 +88,7 @@ def update_active_jobs(time, active_jobs):
             active_jobs.append(job.copy())
 
 
-def RR_3(size):
+def RR_3(size, quantum):
 
     remaining_jobs = size
     avg_wait = 0
@@ -112,7 +112,7 @@ def RR_3(size):
         n+=1
         # print(active_jobs)
         removed_job = False
-        q = 3
+        q = quantum
         ran = 0
         if len(active_jobs) < 1:
             time += 1
@@ -167,97 +167,29 @@ def RR_3(size):
     avg_turn /= size
     print(f"Average -- Turnaround {(avg_turn):3.2f}     Wait {(avg_wait):3.2f}")
 
-
-def RR_2(size):
-
-    remaining_jobs = size
-    avg_wait = 0
-    avg_turn = 0
-    time = 0
-    wait = 0
-    turnaround = 0
-    job_num = 0
-
-    active_jobs = []
-    job_i = 0
-    removed_job = False
-
-    update_active_jobs(time, active_jobs)
-
-    while (remaining_jobs > 0):
-        removed_job = False
-        q = 3
-        ran = 0
-        if len(active_jobs) < 1:
-            time += 1
-            update_active_jobs(time, active_jobs)
-        else:
-            # print(job_i)
-            job = active_jobs[job_i]
-            while (job[0] > 0 and q > 0):
-                if len(job) == 2:
-                    job.append(time)
-                    job.append(job_num)
-                    job_num += 1
-                q -= 1
-                job[0] -= 1
-                time += 1
-                update_active_jobs(time, active_jobs)
-                ran = 1
-
-            if (job[0] == 0 and ran == 1):
-                # print("arrival time:", job[1])
-                turnaround = time-job[1]
-                wait = job[2]-job[1]
-                avg_turn += turnaround
-                avg_wait += wait
-                print(f"Job {job[3]} -- Turnaround {turnaround}  Wait {wait}")
-                remaining_jobs -= 1
-                # print("remaining:", remaining_jobs, "time:", time)
-                turnaround = 0
-                wait = 0
-                active_jobs.pop(job_i)
-                # print(active_jobs)
-                removed_job = True
-
-            # print(job_i)
-            if removed_job:
-                if (job_i >= len(active_jobs)):
-                    job_i = 0
-            elif job_i >= len(active_jobs)-1:
-                job_i = 0
-            else:
-                job_i += 1
-
-    avg_wait /= size
-    avg_turn /= size
-    print(f"Average -- Turnaround {(avg_turn):3.2f}     Wait {(avg_wait):3.2f}")
+    
 
 
 def main():
     """
     """
     SIZE = 0
+    algorithm = "FIFO"
+    quantum = 1
     # check for valid.txt file
-    file = open("test.txt", "r")
+    # file = open("test.txt", "r")
 
     # put what was in.txt file into an array array
-    for x in file:
-        b = x.split()
-        a = [int(b[0]), int(b[1])]
-        matrix.append(a)
-        SIZE += 1
+    
 
-    print(matrix)
-    print(SIZE)
+    # print(matrix)
+    # print(SIZE)
 
-    matrix.sort(key=fifo_key)
-    print("fifo:", matrix)
+    # print("fifo:", matrix)
 
     # FIFO_printout(SIZE)
     # RR_printout(SIZE)
     # RR_2(SIZE)
-    RR_3(SIZE)
     # SRTN_printout(SIZE)
 
     # sort matrix to base config
@@ -265,24 +197,42 @@ def main():
     # check for algorithm
     numOfArgs = len(sys.argv)
     if numOfArgs > 1:
-        if sys.argv[2] == "q":
+        file = open(str(sys.argv[1]), "r")
+    else:
+        file = open("test.txt", "r")
+
+    for x in file:
+        b = x.split()
+        a = [int(b[0]), int(b[1])]
+        matrix.append(a)
+        SIZE += 1
+
+    matrix.sort(key=fifo_key)
+
+    if numOfArgs == 4:
+        if sys.argv[2] == "-q":
             quantum = sys.argv[3]
-        if sys.argv[2] == "p":
+            algorithm == "RR"
+        elif sys.argv[2] == "-p":
             algorithm = sys.argv[3]
 
-        if sys.argv[4] == "q":
+    if numOfArgs == 6:
+        if sys.argv[4] == "-q":
             quantum = sys.argv[5]
+        elif sys.argv[4] == "-p":
+            algorithm = sys.argv[3]
 
-        if algorithm == "SRTN":
-            SRTN_printout()
-        elif algorithm == "RR" or numOfArgs >3:
-            RR_printout()
-        else:
-            FIFO_printout(SIZE)
+    if numOfArgs == 3 or numOfArgs == 5 or numOfArgs > 6:
+        print("Usage: schedSim <job-file.txt> -p <ALGORITHM> -q <QUANTUM>")
+        return 0
 
+    if algorithm == "SRTN":
+        SRTN_printout(SIZE)
+    elif algorithm == "RR" or numOfArgs >3:
+        RR_3(SIZE, (int)(quantum))
     else:
-        # FIFO_printout(SIZE)
-        pass
+        FIFO_printout(SIZE)
+
 
     # check for quantum
 
@@ -304,7 +254,7 @@ if __name__ == "__main__":
     """
     """
     matrix = []
-    quantum = 3
+    quantum = 1
     algorithm = "FIFO"
     main()
 
